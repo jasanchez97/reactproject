8b5087d38de3f5d4988c621b81d6c0766bad59bb
jasanchez97 <juanantoniosanchezmartel@alumno.ieselrincon.es>
2025-02-25 17:17:53 +0000

    Video and image title added as background in Home

--- videogame/src/components/BackgroundVideo/BackgroundVideo.jsx
+++ videogame/src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import videoSrc from "/video/SSBBFinalDestination.webm";
+import videoSrc from "https://www.youtube.com/watch?v=7m3X27TYzok&ab_channel=DyllonStejGaming";
 import "./BackgroundVideo.css"; // Asegúrate de importar correctamente el archivo CSS
 
 function BackgroundVideo() {
   return (
-    <div className="video-wrapper">
+    <div className="video-container">
       <img className="img-title" src="/images/SSBBGameLogo.png"/>
       <video className="background-video" autoPlay loop muted playsInline>
         <source src={videoSrc} type="video/mp4" />
