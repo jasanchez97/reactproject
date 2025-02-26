df0e823ef58c32586a647c7085ae3109530c9724
jasanchez97 <juanantoniosanchezmartel@alumno.ieselrincon.es>
2025-02-26 19:06:51 +0000

    Temporal parch

--- videogame/src/components/Header/Header.jsx
+++ videogame/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./Header.css";
 import useSound from "use-sound";
 import WiiMenuBanner from "../sounds/WiiMenuBanner.mp3";
+import MainScreenSound from "../sounds/MainScreenSound.mp3";
 
 function Header() {
   const [smashLogo, setSmashLogo] = useState("/images/SmashLogo.png");
@@ -17,13 +18,19 @@ function Header() {
 
   const [playSound] = useSound(WiiMenuBanner, { volume: 0.3 });
 
+  const clickButton = () => {
+    playSound2();
+  }
+
+  const [playSound2] = useSound(MainScreenSound, { volume: 0.3});
+
   return (
     <div className="header-main">
       <img className="header-logo" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut} src={smashLogo} id="header-logo" alt="Smash Logo" />
-      <div className="header-buttons">
-        <a href="/home" className="header-button">Inicio</a>
-        <a href="/information" className="header-button">Información</a>
-        <a href="/gallery" className="header-button">Galería</a>
+      <div className="header-buttons" >
+        <a href="/home" className="header-button" onClick={clickButton}>Inicio</a>
+        <a href="/information" className="header-button" onClick={clickButton}>Información</a>
+        <a href="/gallery" className="header-button" onClick={clickButton}>Galería</a>
       </div>
     </div>
   );
